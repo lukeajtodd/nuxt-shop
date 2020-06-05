1cc43c98b526518990d927e8c2746948c9317ef6
Luke Todd <luke@evoluted.email>
2020-06-05 15:12:09 +0100

    Instructed tailwind what to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
+  purge: [
+    './pages/**/*.vue',
+    './components/**/*.vue',
+    './plugins/**/*.vue',
+    './static/**/*.vue',
+    './store/**/*.vue'
+  ],
   theme: {},
   variants: {},
   plugins: []
