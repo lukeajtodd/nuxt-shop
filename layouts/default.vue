d6d33b2939beef6d3fa5fe047adbe1ded239371a
Luke Todd <luke@evoluted.email>
2020-06-05 15:05:46 +0100

    Initial commit, added header, hero and footer

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <main-header />
+    <main>
+      <nuxt />
+    </main>
+    <main-footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Layout',
+  components: {
+    'main-header': Header,
+    'main-footer': Footer
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <hero />
+  </div>
+</template>
+
+<script>
+import Hero from '@/components/Hero.vue'
+
+export default {
+  components: {
+    hero: Hero
+  }
+}
+</script>
